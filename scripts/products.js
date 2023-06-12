bf589a2a34d11dbb34a79ee083c01c7bfa4f4ceb
AlexisYusti18 <alexisyusti18@gmail.com>
2023-06-12 17:13:20 -0300

    page products add input

--- scripts/products.js
+++ scripts/products.js
@@ -1,9 +1,26 @@
 let jerseys = document.getElementById('jerseys-products')
+const searchBar=document.getElementById('searchBar')
 
-jerseysProduscts.map((shirt)=>{
-    jerseys.innerHTML+= `
+
+searchBar.addEventListener('keyup', (e)=>{
+    const serchString= e.target.value
+    const filterClubName=jerseysProduscts.filter((jersey)=>{
+        return(
+            jersey.club.toLowerCase().includes(serchString)
+        )
+    })
+    displayJerseys(filterClubName)
+    // console.log(filterClub);
+})
+
+function displayJerseys(){
+    jerseysProduscts.map((shirt)=>{
+        jerseys.innerHTML+= `
         <div>
             <img class="jerseys" src=${shirt.img}><img/>
-        </div>
-    `
-})
+        </div>`
+    })
+}
+displayJerseys() 
+
+
