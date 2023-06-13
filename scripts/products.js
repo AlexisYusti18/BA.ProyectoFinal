39e473d12ffc2241483ce9a7f3606f36669938ec
AlexisYusti18 <alexisyusti18@gmail.com>
2023-06-13 02:41:55 -0300

    page products add input

--- scripts/products.js
+++ scripts/products.js
@@ -1,26 +1,26 @@
-const jerseys = document.getElementById('jerseys-products')
-const searchBar=document.getElementById('searchBar')
-let filterClubName= [];
+// const jerseys = document.getElementById('jerseys-products')
+// const searchBar=document.getElementById('searchBar')
+// let filterClubName= [];
 
-searchBar.addEventListener('input', (event) => {
-    filterClubName = event.target.value.toLowerCase();
-    showList();
-  });
+// searchBar.addEventListener('input', (event) => {
+//     filterClubName = event.target.value.toLowerCase();
+//     showList();
+//   });
 
-const showList=()=>{
-    jerseys.innerHTML= '';
-    jerseysProducts.filter((item)=>{
+// const showList=()=>{
+//     jerseys.innerHTML= '';
+//     jerseysProducts.filter((item)=>{
     
-        return(
-            item.club.toLowerCase().includes(filterClubName)
-            )
-    }).forEach((e)=>{
-        const img= document.createElement('img')
-        img.innerHTML= `<img class="jerseys" src=${e.img}><img/>`
-        jerseys.appendChild(img)
-    })
-}
-showList()
+//         return(
+//             item.club.toLowerCase().includes(filterClubName)
+//             )
+//     }).forEach((e)=>{
+//         const img= document.createElement('img')
+//         img.innerHTML= `<img class="jerseys" src=${e.img}><img/>`
+//         jerseys.appendChild(img)
+//     })
+// }
+// showList()
 // searchBar.addEventListener('keyup', (e)=>{
 //     let arr= []
 //     const searchString= e.target.value
@@ -38,4 +38,26 @@ showList()
 //     })
 
 
+// const searchBar=document.getElementById('searchBar')
+buscarElemento()
+  // Función para buscar elementos en el array
+  function buscarElemento() {
+    // Obtener el valor ingresado en la barra de búsqueda
+    let consulta = document.getElementById("searchBar").value.toLowerCase()
+    console.log(consulta);
+    // Filtrar el array de elementos según la consulta
+    var resultados = jerseysProducts.filter((elemento)=> {
+        // console.log(elemento.club);
+      return elemento.club.toLowerCase().includes(consulta);
+    });
+    
+    // Mostrar los resultados en la página
+    var resultadoHTML = "";
+    resultados.forEach(function(resultado) {
+      resultadoHTML += '<div>' +
+                       '<img class="jerseys" src="' + resultado.img + '">' +                       
+                       '</div>';
+    });
+    document.getElementById("jerseys-products").innerHTML = resultadoHTML;
+  }
 
